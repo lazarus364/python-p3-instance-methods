92dc7a3b12a9c7bf4ea1d7c2f232bb3052f494a5
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2025-01-16 18:04:03 +0300

    python

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,15 @@
 
 class Dog:
     # Class body goes here
+   # Assuming this is part of your existing Dog class in lib/dog.py  
+    def bark(self):  
+        print("Woof!")  
+
+    def sit(self):  
+        print("The dog is sitting.")  
 
     #Instance method definition
-    pass
+my_dog = Dog()  
+my_dog.bark()  
+my_dog.sit()
+
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,14 @@
 
 class Person:
     # Class body goes here
-
-    #Instance method definition
-    pass
+    # lib/person.py
+# Create a new file lib/person.py    
+    def talk(self):  
+        print("Hello World!")  # This method prints "Hello World!"  
+        
+    def walk(self):  
+        print("The person is walking.")
+        
+person = Person()  
+person.talk()  # Output: Hello World!  
+person.walk()
